a0c3446307b3db152644fdc72ed17f3935b8b441
Ryan Slobojan <straxus@gmail.com>
2021-10-20 12:24:41 -0400

    Cleanroom reimplementation of Google Pass registration function
    * New function is at a new URL - /googlesign
    * Implemented proper CORS protection
    * Heavily documented code to ease readability
    * Reimplemented as single-function all-inline to ease
      understanding of logic flow
    * Once this is verified working, will clean up remaining repo code
    * Removed previous license header because this is now a cleanroom
      reimplementation of our function logic, and no longer borrows in
      any substantial way from the Google example code

--- functions/index.js
+++ functions/index.js
@@ -1,45 +1,124 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-const express = require('express');
 const functions = require('firebase-functions');
-const bodyParser = require('body-parser');
-const { loyaltyRoutes } = require('./loyalty');
-const { covidcardRoutes } = require('./covidcard');
+const config = require('./config');
+const jwt = require('jsonwebtoken');
 
-const cors = require('cors');
+// Allow requests from *.vaccine-ontario.ca, and from the Google Cloud functions domain that this function runs on
+const cors = require('cors')({ origin: [/\.vaccine-ontario\.ca$/, "https://us-central1-grassroots-gpay.cloudfunctions.net"] });
 
-const app = express();
-app.use(cors());
-app.options('*', cors());
+exports.googlesign = functions.https.onRequest((request, response) => {
+    /*
+        VERY IMPORTANT WARNING!!!!! VERY IMPORTANT WARNING!!!!! VERY IMPORTANT WARNING!!!!!
+    
+        The Google Pay API requires us to sign the entire Google Pass contents with this function - this is different
+        from Apple Wallet which signs hashes of the content. This means that it is UNAVOIDABLE that potentially
+        personally identifiable information will flow through this function if we wish to provide this functionality,
+        since the SHC QR code contains person name and birthdate. Given the unavoidability of transmission, it is
+        ABSOLUTELY VITAL that NO logging of any of that data happens within this function!! This function has been
+        implemented so that all of the logic is inliine and as simple as possible to reduce the chance of unintentional
+        logging and to make understanding of the overall logic as simple as possible for readers so it can be verified
+        without much effort that no PII recording occurs within this function.
+    
+        VERY IMPORTANT WARNING!!!!! VERY IMPORTANT WARNING!!!!! VERY IMPORTANT WARNING!!!!!
+    */
+    cors(request, response, async (err) => {
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+        // Kick out anything that is not valid according to our CORS rules
+        if (err) {
+            // Denied by CORS/error with CORS configuration
+            console.error("CORS blocked request -> ", err);
+            response.status(403).send("Forbidden by CORS");
+            return;
+        }
 
-const routers = {
-  '/loyalty': loyaltyRoutes,
-  '/covidcard': covidcardRoutes
-};
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        // STEP 1: declare a bunch of constants at the top, C-style, to make it easier to understand
+        // the logic below by using descriptive variable names.
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        const qrCode = request.body.payloadBody.rawData;
+        const shcReceipt = request.body.payloadBody.shcReceipt;
+        const vaccinations = shcReceipt.vaccinations;
 
-// register apis with an additional /api path
-Object.entries(routers).forEach(([path, router]) => {
-  app.use(path, router);
+        const passIssuerId = config.issuerIdCovidcard;
+        const passIssuerEmail = config.credentials.client_email;
+        const passIssuerPrivateKey = config.credentials.private_key;
 
-  // also register the /api prefix for rewrites
-  app.use(`/api${path}`, router);
+        // The pass ID combines both the issuer and our unique pass ID
+        const passId = `${passIssuerId}.${request.body.id}`;
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        // STEP 2: Separate out our vaccination records into the format Google Pay wants them in.
+        // Loop through each our vaccination records and prepare them for adding to the pass.
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        const passVaccinationRecords = [];
+        for (let i = 0; i < vaccinations.length; i++) {
+            const vaccination = vaccinations[i];
+            const vaccinationRecord = {
+                doseDateTime: vaccination.vaccinationDate,
+                manufacturer: vaccination.vaccineName,
+                provider: vaccination.organization,
+                doseLabel: `Dose ${i + 1}`
+            };
+            
+            // Dump this constructed object int our overall list of vaccination records for
+            // inclusion in the Google Pay pass
+            passVaccinationRecords.push(vaccinationRecord);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        // STEP 3: Construct the body of the Google Pay pass we will return to the user.
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        const passContents = {
+            id: passId,
+            issuerId: passIssuerId,
+            title: `COVID-19 Vaccination Card, ${shcReceipt.cardOrigin}`,
+            patientDetails: {
+                dateOfBirth: shcReceipt.dateOfBirth,
+                patientName: shcReceipt.name
+            },
+            vaccinationDetails: {
+                vaccinationRecord: passVaccinationRecords
+            },
+            barcode: {
+                type: 'qrCode',
+                value: qrCode,
+            },
+            cardColorHex: '#FFFFFF',
+            logo: {
+                sourceUri: {
+                    description: 'Grassroots',
+                    uri: 'https://www.gstatic.com/images/icons/material/system_gm/2x/gpp_good_black_48dp.png'
+                }
+            },
+        };
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        // STEP 4: Create our JWT Claims object which will be used to sign this Google Pass. Please note that
+        // the body of the pass is included in this claims object.
+        //
+        // The fields 'aud', 'iat', and 'typ' are constants defined by Google in
+        // https://developers.google.com/pay/passes/guides/covid-cards/get-started
+        // however that URL is only accessible if you have been granted access to the COVID card API -
+        // it is a 404 otherwise.
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        const claims = {
+            aud: 'google',
+            iat: 1620349632,
+            typ: 'savetogooglepay',
+            iss: passIssuerEmail,
+            payload: {covidCardObjects: [passContents]}
+        };
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        // STEP 5: Sign our JWT and create our return token
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        const token = jwt.sign(claims, passIssuerPrivateKey, {algorithm: 'RS256'});
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        // STEP 6: Send this token back as our response now that signing is complete
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        response.json({token});
+
+        // Log only the pass ID so we have an indication in the logs of a successful run by this function
+        console.info(`Pass with ID ${passId} successfully created and returned to user`);
+    });
 });
-
-exports.api = functions.https.onRequest(app);
