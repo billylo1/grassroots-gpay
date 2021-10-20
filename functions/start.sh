a8d7c8b186b90c368ab6e6fa9f6fd52129e73487
Ryan Slobojan <straxus@gmail.com>
2021-10-20 15:03:01 -0400

    Removed no-longer-needed previous dev code
    * Deleted initial code which was based on a Google sample app
    * Overall code footprint is now dramatically smaller
    * Will also clean up the config class as soon as new function is verified working

--- functions/start.sh
+++
@@ -1,5 +0,0 @@
-export GCP_CREDENTIALS=/Users/billylo/web/grassroots-gpay/key.json
-export GOOGLE_PAY_ISSUER_ID=3388000000018205867 
-export LOYALTY_PROGRAM=shc-receipt
-export LOYALTY_WEBSITE='Vaccine-Ontario.ca'
-firebase emulators:start --only functions
